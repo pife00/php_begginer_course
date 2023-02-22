baa4dd827504ce239e875aba815507d7ae9d9a31
pife00 <pife00@gmail.com>
2023-02-22 15:36:45 -0500

    14

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -31,9 +31,9 @@
         <div class="hidden sm:ml-6 sm:block">
           <div class="flex space-x-4">
             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-            <a href="/mypage" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Home</a>
+            <a href="/mypage" class="<?php if($_SERVER['REQUEST_URI'] === '/mypage/'){echo 'bg-gray-700 text-white';} ?> text-white  px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Home</a>
 
-            <a href="/mypage/about.php" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
+            <a href="/mypage/about.php" class="<?php if($_SERVER['REQUEST_URI'] === '/mypage/about.php'){echo 'bg-gray-700 text-white';} ?> text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
 
             <a href="/projects" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>
 
